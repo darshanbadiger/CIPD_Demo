a1c10a47875b8c706f534427e64a22cb9379f842
darshanbadiger <darshanabadiger@gmail.com>
2020-04-04 19:27:57 +0530

    Add files via upload

--- CIPD_practice/urls.py
+++ CIPD_practice/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path,include
 from firstapp import views
 from demo import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('firstapp.urls')),
     path('', include('demo.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
